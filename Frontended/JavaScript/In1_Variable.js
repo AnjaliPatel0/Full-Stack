60a4e2bfbb69d15fb9b9ba2da3c524fcda705dc5
AnjaliPatel0 <anjalipatela78@gmail.com>
2026-09-17 08:38:52 +0530

    JS

---
+++ Frontended/JavaScript/In1_Variable.js
@@ -0,0 +1,30 @@
+// JS -> JS  is a high level interprted programming
+// language primarily used for web
+// * Versatile and Dynamic
+// * Cliend-side Scripting
+// * Highly Flexible
+// * Supports Functional programming
+// * Object-oriented prog
+console.log("Welcome to WS")
+console.log(20+30)
+
+// Variable -> VAriables are used to store and representdata values
+// can be access out of block can declare multiple times with same name
+// var n=10;
+// console.log(n)
+// var m ="hello"
+// console.log(m)
+// n=50;
+// console.log(n)
+
+
+//  let keyword variable cannot declare multiple times with same name
+// can't access out of block
+// let n=10;
+let n=10,m=5,a="hello";  // multiple initialise value
+console.log(a)
+
+const x=10; // const is define only once and can't change
+console.log(x)
+// x=50;
+// console.log(x)
---
+++ Frontended/JavaScript/In2_StringAndTemplateLiteral.js
@@ -0,0 +1,35 @@
+// Strings And Template Literals
+let n=10;
+let w=`Welcome ${n} to ${20+10} WS`
+console.log(w)
+
+
+// DataTypes
+// Primitive -> Number, String, Boolean, Null,undefined, symlbol,bigInt
+// Non primitive -> Object
+let num=20,num1='20',num2=20.5;
+console.log( num ,typeof num)
+console.log( num1 ,typeof num1,num2,typeof num2)
+
+// boolean
+let s=true;
+console.log(s,typeof s)
+console.log(s+5)
+
+// NULL
+let nu=null;
+console.log(typeof nu)
+
+// undefined
+let x;
+console.log(x, typeof x)
+
+// Symbol -> it define unique value
+let uid=Symbol('123')
+let cid=Symbol('123')
+console.log(uid==cid)
+
+// BigInt
+let bigInt=8888888888888n;
+console.log(bigInt,typeof bigInt)
+
